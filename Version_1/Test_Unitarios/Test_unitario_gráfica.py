137a699f1e73b40625ca5efb66d15eefc8fabd76
Michail2007 <74192177+Michail2007@users.noreply.github.com>
2025-10-17 12:48:49 +0200

    Create Test_unitario_gráfica.py

---
+++ Version_1/Test_Unitarios/Test_unitario_gráfica.py
@@ -0,0 +1,3 @@
+print (linea)
+#He integrado esto en la definición de read_serial del código en python principal para que me ponga en el terminal los datos sin procesar que envia el arduino satélite.
+#Me ha permitido encontrar un error donde había escrito 'e' en vez de "e", por lo que el programa de python no entendia que había un error.
